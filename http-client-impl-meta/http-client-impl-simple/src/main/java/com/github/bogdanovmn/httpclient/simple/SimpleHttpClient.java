8b5d0f532d3a13dd3598771df6f4f3d80d8e1289
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2018-08-25 15:57:25 +0300

    add download file method

--- http-client-impl-meta/http-client-impl-simple/src/main/java/com/github/bogdanovmn/httpclient/simple/SimpleHttpClient.java
+++ http-client-impl-meta/http-client-impl-simple/src/main/java/com/github/bogdanovmn/httpclient/simple/SimpleHttpClient.java
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -38,6 +39,12 @@ public class SimpleHttpClient implements HttpClient {
 				? "UTF-8"
 				: response.getEntity().getContentEncoding().getValue()
 		);
+	}
+
+	public InputStream downloadFile(String url) throws IOException {
+		return httpClient.execute(
+			new HttpGet(urlPrefix + url)
+		).getEntity().getContent();
 	}
 
 	public Date getLastModified(String url) throws IOException {
